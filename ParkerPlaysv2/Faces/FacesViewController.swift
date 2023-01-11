d9398b10613abea37241d1cc1c640ac7cd020346
Suzie <reorsusie@gmail.com>
2023-01-11 15:10:14 -0800

    FacesVC - adjusted constraints to include 'Add Photos' button - checked iPhone14 and SE

--- ParkerPlaysv2/Faces/FacesViewController.swift
+++ ParkerPlaysv2/Faces/FacesViewController.swift
@@ -14,22 +14,24 @@ class FacesViewController: UIViewController {
     let addImageButton = UIButton()
     var imagePicker = UIImagePickerController()
     var sound = PlaySound()
+    let label = AnimalLabelView()
     
     override func viewDidLoad() {
        super.viewDidLoad()
         setupBackground()
         setupHomeButton()
         setupCard()
+        setupLabel()
         setupAddImageButton()
     }
         
     
-    func setupBackground(){
+    func setupBackground() {
         view.addSubview(background)
         background.image = UIImage(named: "gradientbg")
     }
     
-    func setupHomeButton(){
+    func setupHomeButton() {
         view.addSubview(homeButton)
         homeButton.setupHome()
         homeButton.addTarget(self, action: #selector(toHome), for: .touchUpInside)
@@ -51,20 +53,44 @@ class FacesViewController: UIViewController {
         //CONSTRAINT
         card.translatesAutoresizingMaskIntoConstraints = false
         card.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        card.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.7).isActive = true
+        card.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.65).isActive = true
         card.topAnchor.constraint(equalTo: homeButton.bottomAnchor).isActive = true
         card.widthAnchor.constraint(equalToConstant: 315).isActive = true
     }
-    func setupAddImageButton(){
+    func setupAddImageButton() {
         view.addSubview(addImageButton)
+        addImageButton.backgroundColor = UIColor(red: 79/255, green: 151/255, blue: 253/255, alpha: 1)
+        addImageButton.tintColor = .white
+        addImageButton.setTitle("ADD PHOTOS", for: .normal)
+        addImageButton.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        addImageButton.layer.cornerRadius = 22
+        addImageButton.addTarget(self, action: #selector(playSound), for: .touchUpInside)
         
         addImageButton.translatesAutoresizingMaskIntoConstraints = false
         addImageButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
         addImageButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
-        addImageButton.topAnchor.constraint(equalTo: card.bottomAnchor, constant: 15).isActive = true
+        addImageButton.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 10).isActive = true
         // this sets the button to be centered on the screen
         addImageButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         }
+    
+    @objc func playSound() {
+            sound.soundFile = "buttonclick1"
+            sound.playSound()
+        }
+    
+    func setupLabel() {
+        view.addSubview(label)
+        label.animalLabel.text = "grandpa"
+        
+        //CONSTRAINTS
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.widthAnchor.constraint(equalToConstant: 315).isActive =  true
+        label.heightAnchor.constraint(equalToConstant: 65).isActive = true
+        label.topAnchor.constraint(equalTo: card.bottomAnchor, constant: 10).isActive = true
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+    }
     
     
     
